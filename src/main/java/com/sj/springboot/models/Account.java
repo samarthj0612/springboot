1517b2467929c88d5c379e4a9222a7706b54d7e4
Samarth Jain <samarth.brandsamashers@gmail.com>
2024-07-27 23:36:24 +0530

    Integrate h2-database

--- src/main/java/com/sj/springboot/models/Account.java
+++ src/main/java/com/sj/springboot/models/Account.java
@@ -1,17 +1,8 @@
 package com.sj.springboot.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.*;
+import jakarta.persistence.*;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
 public class Account {
 
     @Id
@@ -20,5 +11,46 @@ public class Account {
     private String accountNumber;
     private Double balance;
 
+    public Account() {
+    }
+
+    public Account(Long id, String accountNumber, Double balance) {
+        this.id = id;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
     // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", accountNumber='" + accountNumber + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
 }
